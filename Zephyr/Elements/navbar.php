74f50a77dc606100de10c7b219deca3f094ebbfb
emeraldDev48 <emeraldDev48>
2022-07-05 21:11:38 +0200

    Added:  - Navbar for Zephyr "admin" / control panel.

---
+++ Zephyr/Elements/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar">
+	<div class="logo"></div>
+	<ul class="navbar-nav">
+		<li class="navbar-item">
+			<i class="fa-solid fa-earth-americas"></i>
+			<span>Websites</span>
+		</li>
+		<li class="navbar-item">
+			<i class="fa-solid fa-plus"></i>
+			<span>Items</span>
+		</li>
+		<li class="navbar-item">
+			<i class="fa-solid fa-gear"></i>
+			<span>Settings</span>
+		</li>
+	</ul>
+
+	<ul class="bottom-nav">
+		<li class="bottom-item">
+			<i class="fa-solid fa-circle-question"></i>
+		</li>
+		<li class="bottom-item">
+			<i class="fa-solid fa-magnifying-glass"></i>
+		</li>
+	</ul>
+</nav>
+
+<header class="header">
+    <div class="devices">
+        <div class="device selected">
+            <i class="fa-solid fa-laptop"></i>
+            <div class="tooltip">
+                <span class="title">Desktop</span>
+                <p>Desktop styles apply at all brakpoints, unless they're edited at a larger or smaller breakpoint start your styling here.</p>
+            </div>
+        </div>
+        <div class="device">
+            <i class="fa-solid fa-tablet-button"></i>
+            <div class="tooltip">
+                <span class="title">Tablet</span>
+                <p>Tablet styles apply at all brakpoints, unless they're edited at a larger or smaller breakpoint start your styling here.</p>
+            </div>
+        </div>
+        <div class="device">
+            <i class="fa-solid fa-mobile"></i>
+            <div class="tooltip">
+                <span class="title">Mobile</span>
+                <p>Mobile styles apply at all brakpoints, unless they're edited at a larger or smaller breakpoint start your styling here.</p>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ Zephyr/index.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <title>Welcome to Zephyr</title>
+    <link rel="stylesheet" href="/Zephyr/styles/style.css">
+    <link rel="stylesheet" href="/Zephyr/styles/navbar.css">
+
+    <script src="https://kit.fontawesome.com/c08158a64a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<?php include './Elements/navbar.php' ?>
+</body>
+</html>
